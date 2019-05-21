ebfd7a3faea4bd2d727e9fb83f34408c1eaaa10c
Namit Nathwani <namsnath@gmail.com>
2019-05-21 18:41:09 +0530

    Added options to make ext.js routes work with large data

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,10 @@ import bodyParser from 'body-parser';
 import logger from 'morgan';
 import mongoose from 'mongoose';
 
+import ext from './routes/ext';
+
 // and create our instances
 const app = express();
-const router = express.Router();
 
 mongoose.connect("mongodb://localhost:27017/FFCS");
 const db = mongoose.connection;
@@ -17,17 +18,15 @@ db.once('open', function () {
 
 // set our port to either a predetermined port number if you have set it up, or 3001
 const API_PORT = process.env.API_PORT || 3001;
-// now we should configure the API to use bodyParser and look for JSON data in the request body
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: false }));
 app.use(logger('dev'));
 
-// now we can set the route path & initialize the API
-router.get('/', (req, res) => {
+app.use('/ext', ext);
+
+app.get('/', (req, res) => {
 	res.json({ message: 'Hello, World!' });
 });
-
-// Use our router configuration when we call /api
-app.use('/api', router);
 
 app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));

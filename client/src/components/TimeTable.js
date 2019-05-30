668c96819ef7917e2b84e33274c7b8fa882f44ef
sudo-nan0-RaySK <saksham.sethi2017@vitstudent.ac.in>
2019-05-30 16:28:29 +0530

    update: Initialised TimeTable component

---
+++ client/src/components/TimeTable.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+// Removed logo since we don't need it anymore
+class TimeTable extends Component {
+  render() {
+    return (
+      <div classname="container">
+        <h1>Here will be our timetable component</h1>
+      </div>
+    );
+  }
+}
+export default TimeTable;

087e8e145664e9efe455e4b6adbf910d3781ea96
sudo-nan0-RaySK <saksham.sethi2017@vitstudent.ac.in>
2019-05-31 02:22:30 +0530

    update: Made basic layout  for timtable component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 // Removed logo since we don't need it anymore
 import './App.css';
+import './components/TimeTable'
+import TimeTable from './components/TimeTable';
 class App extends Component {
 	render() {
 		return (
 			<div className="container">
 				<h1>Hello World!</h1>
 				<p>I just created my first React App</p>
+				<TimeTable/>
 			</div>
 		);
 	}

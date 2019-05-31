b18d7c2a1c91d59b44ae1e4d14cae8468324c9d3
Namit Nathwani <namsnath@gmail.com>
2019-06-01 02:34:27 +0530

    More trial values in react placeholder for auth

--- client/src/App.js
+++ client/src/App.js
@@ -3,33 +3,35 @@ import "whatwg-fetch";
 // Removed logo since we don't need it anymore
 import "./App.css";
 class App extends Component {
-  constructor(state) {
-    super(state);
-    this.state = {
-      data: null,
-      error: null
-    };
-    // this.loadAccountDetailsFromServer();
-  }
+	constructor(state) {
+		super(state);
+		this.state = {
+			data: {},
+			error: null
+		};
+	}
 
-  async componentWillMount() {
-    let k;
-    var res = await fetch("/account");
-    var data = await res.json();
-    k = data.display_name;
-    console.log("k is ", k);
-    this.setState({ data: k });
-  }
-  render() {
-    console.log("render called");
-    return (
-      <div className="container">
-        <h1>Hello World!</h1>
-        <p>I just created my first React App</p>
-        <p>{this.state.data}</p>
-        <p>Yo</p>
-      </div>
-    );
-  }
+	async componentWillMount() {
+		var res = await fetch("/account");
+		var parsed = await res.json();
+
+		if(parsed.authenticated)
+			this.setState({ data: parsed });
+		else
+			this.setState({ error: "Not Authenticated" });	
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<h1>Hello World!</h1>
+				<p>I just created my first React App</p>
+				<p>{ this.state.data.display_name }</p>
+				<img src={ this.state.data.picture }></img>
+
+				<p>{ this.state.error }</p>
+			</div>
+		);
+	}
 }
 export default App;

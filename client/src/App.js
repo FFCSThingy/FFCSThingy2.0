a39179475ae5741bcf86d7a92aaa48d48307c38c
sudo-nan0-RaySK <saksham.sethi2017@vitstudent.ac.in>
2019-05-31 18:18:32 +0530

    fix: routing and data race while using fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,35 @@
-import React, { Component } from 'react';
-import 'whatwg-fetch';
+import React, { Component } from "react";
+import "whatwg-fetch";
 // Removed logo since we don't need it anymore
-import './App.css';
+import "./App.css";
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			data: null,
-			error: null
-		};
-		// this.loadAccountDetailsFromServer();
-	}
+  constructor(state) {
+    super(state);
+    this.state = {
+      data: null,
+      error: null
+    };
+    // this.loadAccountDetailsFromServer();
+  }
 
-	componentWillMount() {
-		this.loadAccountDetailsFromServer();
-	}
-
-	loadAccountDetailsFromServer = () => {
-		fetch('/account')
-			.then(data => data.json())
-			.then((res) => {
-				if (!res.success) this.setState({ error: res.error });
-				else this.setState({ data: res.display_name });
-			});
-	}
-	
-	render() {
-		return (
-			<div className="container">
-				<h1>Hello World!</h1>
-				<p>I just created my first React App</p>
-				<p>{ this.state.data }</p>
-			</div>
-		);
-	}
+  async componentWillMount() {
+    let k;
+    var res = await fetch("/account");
+    var data = await res.json();
+    k = data.display_name;
+    console.log("k is ", k);
+    this.setState({ data: k });
+  }
+  render() {
+    console.log("render called");
+    return (
+      <div className="container">
+        <h1>Hello World!</h1>
+        <p>I just created my first React App</p>
+        <p>{this.state.data}</p>
+        <p>Yo</p>
+      </div>
+    );
+  }
 }
 export default App;

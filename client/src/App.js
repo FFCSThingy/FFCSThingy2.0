6cd71969fd9008d851af399cfce7084bec681948
Namit Nathwani <namsnath@gmail.com>
2019-05-31 17:12:31 +0530

    Tried displaying account details on root

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
+import 'whatwg-fetch';
 // Removed logo since we don't need it anymore
 import './App.css';
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			data: null,
+			error: null
+		};
+		// this.loadAccountDetailsFromServer();
+	}
+
+	componentWillMount() {
+		this.loadAccountDetailsFromServer();
+	}
+
+	loadAccountDetailsFromServer = () => {
+		fetch('/account')
+			.then(data => data.json())
+			.then((res) => {
+				if (!res.success) this.setState({ error: res.error });
+				else this.setState({ data: res.display_name });
+			});
+	}
+	
 	render() {
 		return (
 			<div className="container">
 				<h1>Hello World!</h1>
 				<p>I just created my first React App</p>
+				<p>{ this.state.data }</p>
 			</div>
 		);
 	}

e74d22e2497bc70d548bdbe06218a2913fd75cb3
Namit Nathwani <namsnath@gmail.com>
2019-06-01 23:47:59 +0530

    Setup basic heatmap route for courses

--- backend/routes/course.js
+++ backend/routes/course.js
@@ -10,6 +10,12 @@ const router = express.Router();
 router.use(express.json({ limit: '50mb' }));
 router.use(express.urlencoded({ limit: '50mb', extended: false }));
 
-
+router.post('/getFullHeatMap', async (req, res, next) => {
+	console.log(req.body.timestamp);
+	if(!req.body.timestamp)	// TODO: Add timestamp comparison here 
+		res.json({ success: true, data: await course.getFullHeatmap() });
+	else
+		res.status(304).json({ success: true, message: "Up To Date" });	
+});
 
 module.exports = router;
--- backend/utility/courseUtility.js
+++ backend/utility/courseUtility.js
@@ -5,3 +5,18 @@ import path from 'path';
 import Curriculum from '../models/Curriculum';
 import User from '../models/User';
 import Course from '../models/Course';
+
+module.exports.getFullHeatmap = () => {
+	return new Promise((resolve, reject) => {
+		Course.aggregate([
+			{
+				$project: {
+					_id: 0
+				}
+			}
+		], function(err, doc) {
+			if(err) return reject(err);
+			return resolve(doc);
+		});
+	});
+}

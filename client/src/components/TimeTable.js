0748bd4c8d91d47df57ca66b3c0eec2c7cc38e06
ffardeen <fardeenasif.khan2018@vitstudent.ac.in>
2019-06-02 04:36:53 +0530

    Updated Time Table Added media query to enable horizontal scroll when screen width < 1050px Few changes to the design

--- client/src/components/TimeTable.js
+++ client/src/components/TimeTable.js
@@ -15,26 +15,31 @@ class TimeTable extends Component {
   render() {
     return (
       <div className="containerGrid">
-        <div className="sectionGridMorning">
-          {this.getLabelElement("THEORY \n HOURS", "grayLabel theory")}
+        <div className="sectionGridDays">
+		{this.getLabelElement("THEORY \n HOURS", "grayLabel theory")}
+		{this.getLabelElement("LAB \n HOURS", "grayLabel lab")}
+		{this.getLabelElement("MONDAY", "grayLabel mon")}
+		{this.getLabelElement("TUESDAY", "grayLabel tue")}
+		{this.getLabelElement("WEDNESDAY", "grayLabel wed")}
+		{this.getLabelElement("THURSDAY", "grayLabel thu")}
+		{this.getLabelElement("FRIDAY", "grayLabel fri")}
+		</div>
+		<div className="sectionGridMorning">
+
           {this.getLabelElement("8:00AM\nTO\n8:50AM", "purpleLabel theory")}
           {this.getLabelElement("9:00AM\nTO\n9:50AM", "purpleLabel theory")}
           {this.getLabelElement("10:00AM\nTO\n10:50AM", "purpleLabel theory")}
           {this.getLabelElement("11:00AM\nTO\n11:50AM", "purpleLabel theory")}
           {this.getLabelElement("12:00PM\nTO\n12:50PM", "purpleLabel theory")}
           {this.getLabelElement("NO THEORY \n SLOTS", "purpleLabel theory")}
-          {this.getLabelElement("LAB \n HOURS", "grayLabel lab")}
+
           {this.getLabelElement("8:00AM\nTO\n8:45AM", "blueLabel lab")}
           {this.getLabelElement("8:46AM\nTO\n9:30AM", "blueLabel lab")}
           {this.getLabelElement("10:00AM\nTO\n10:45AM", "blueLabel lab")}
           {this.getLabelElement("10:46AM\nTO\n11:30AM", "blueLabel lab")}
           {this.getLabelElement("11:31AM\nTO\n12:15PM", "blueLabel lab")}
           {this.getLabelElement("12:16PM\nTO\n1:00PM", "blueLabel lab")}
-          {this.getLabelElement("MONDAY", "grayLabel mon")}
-          {this.getLabelElement("TUESDAY", "grayLabel tue")}
-          {this.getLabelElement("WEDNESDAY", "grayLabel wed")}
-          {this.getLabelElement("TURSDAY", "grayLabel thu")}
-          {this.getLabelElement("FRIDAY", "grayLabel fri")}
+
 
 
           <SlotElement isEmpty="empty" slotName="A1L1" />
@@ -77,7 +82,8 @@ class TimeTable extends Component {
           
         </div>
         <div className=" grayLabel">
-          {this.getLabelElement("L\nU\nN\nC\nH")}
+		  {this.getLabelElement("L\nU\nN\nC\nH")}
+		  
         </div>
         <div className="sectionGridEvening">
           {this.getLabelElement("2:00PM\nTO\n2:50PM", "purpleLabel theory")}
@@ -85,7 +91,7 @@ class TimeTable extends Component {
           {this.getLabelElement("4:00PM\nTO\n4:50PM", "purpleLabel theory")}
           {this.getLabelElement("5:00PM\nTO\n5:50PM", "purpleLabel theory")}
           {this.getLabelElement("6:00PM\nTO\n6:50PM", "purpleLabel theory")}
-          {this.getLabelElement("7:00PM\nTO\n7:50PM", "purpleLabel theory")}
+          {this.getLabelElement("7:00PM\nTO\n7:50PM", "purpleLabel theory6")}
           {this.getLabelElement("2:00PM\nTO\n2:45PM", "blueLabel lab")}
           {this.getLabelElement("2:46PM\nTO\n2:30PM", "blueLabel lab")}
           {this.getLabelElement("4:00PM\nTO\n4:45PM", "blueLabel lab")}

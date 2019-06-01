71883e54dca1d9df786b0f852acca28d60e49e8f
Namit Nathwani <namsnath@gmail.com>
2019-06-01 12:53:51 +0530

    Initialised course files

---
+++ backend/utility/courseUtility.js
@@ -0,0 +1,7 @@
+import fs from 'fs';
+import path from 'path';
+
+// Models
+import Curriculum from '../models/Curriculum';
+import User from '../models/User';
+import Course from '../models/Course';

1955ae7b74af751a42b6497dc8a91846810df654
Namit Nathwani <namsnath@gmail.com>
2019-06-04 14:45:40 +0530

    Setup a ton of course routes and required functions

--- backend/routes/course.js
+++ backend/routes/course.js
@@ -10,12 +10,80 @@ const router = express.Router();
 router.use(express.json({ limit: '50mb' }));
 router.use(express.urlencoded({ limit: '50mb', extended: false }));
 
-router.post('/getFullHeatMap', async (req, res, next) => {
+router.get('/getFullHeatMap', async (req, res, next) => {
 	console.log(req.body.timestamp);
 	if(!req.body.timestamp)	// TODO: Add timestamp comparison here 
 		res.json({ success: true, data: await course.getFullHeatmap() });
 	else
 		res.status(304).json({ success: true, message: "Up To Date" });	
 });
+
+router.get('/getCourseList', async (req, res, next) => {
+	try {
+		var data = await course.getCourseList();
+		res.json({ success: true, data: data });
+	} catch(err) {
+		res.status(500).json({ success: false, message: '/getCourseList failed' });
+	}
+});
+
+router.post('/parseCourses', async (req, res, next) => {
+	if(req.body.password != "SuckOnDeezNumbNutz")
+		res.status(403).json({ success: false, message: "Get the password right, bitchface." });	
+});
+
+router.get('/parseCourses', async (req, res, next) => {
+	try {
+		var parsedData = await course.parseXLSX();
+		res.json(parsedData);
+	} catch(err) {
+		res.status(500).json({ success: false, message: '/parseCourses failed' });
+		console.log(err);
+	}
+});
+
+router.get('/addCoursesToDB', async (req, res, next) => {
+	try {
+		var courses = await course.parseXLSX();
+
+		var actions = courses.map(course.addCourseToDB);
+		var results = Promise.all(actions);
+
+		res.json(await results);
+
+	} catch(err) {
+		res.status(500).json({ success: false, message: '/addCoursesToDB failed' });
+		console.log(err);
+	}
+});
+
+router.get('/getCourseByDetails/:code/:type/:faculty/:venue', async (req, res, next) => {
+	try {
+		var data = {
+			code: req.params.code,
+			course_type: req.params.type,
+			faculty: req.params.faculty,
+			venue: req.params.venue
+		};
+
+		res.json(await course.getCourseDetails(data));
+	} catch (err) {
+		res.status(500).json({ success: false, message: '/getCourseByDetails failed' });
+		console.log(err);
+	}
+});
+
+router.get('/getCourseByID/:id', async (req, res, next) => {
+	try {
+		var data = {
+			_id: req.params.id,
+		};
+
+		res.json(await course.getCourseDetails(data));
+	} catch (err) {
+		res.status(500).json({ success: false, message: '/getCourseByID failed' });
+		console.log(err);
+	}
+})
 
 module.exports = router;

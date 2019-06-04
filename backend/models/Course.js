a44d016e0043413c7697cccf32017fa52f2d9a59
Namit Nathwani <namsnath@gmail.com>
2019-06-04 14:42:34 +0530

    Cleaned up Course model and added defaults

--- backend/models/Course.js
+++ backend/models/Course.js
@@ -8,13 +8,9 @@ var courseSchema = mongoose.Schema({	// From the data given in the sheet
 	slot: String, 
 	faculty: String,
 	venue: String,
-	count: Number,
-	course_count: Number,
-	slot_count: Number,
-	faculty_count: Number,
-	specific_percent: Number,
-	slot_percent: Number,
-	faculty_percent: Number
+	count: { type: Number, default: 0 },
+	percent: { type: Number, default: 0 },
+	timestamp: { type: Date, default: Date.now() },
 });
 
 module.exports = mongoose.model("Course", courseSchema);

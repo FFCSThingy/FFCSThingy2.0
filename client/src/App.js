06c70ee1946ed3eea2bd5c7c693376375b68605c
ffardeen <fardeenasif.khan2018@vitstudent.ac.in>
2019-06-05 13:48:36 +0530

    Components merged

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,42 @@
-import React, { Component } from "react";
+/*Components Merged
+constructor function(line 33) is commented for now.
+gives error when enabled.
+*/
+
+import React from 'react';
+import Search from './components/searchBar';
+import Course from './components/course.js';
+//import Table from './components/table';
+import CourseTable from './components/coursetable';
 import "whatwg-fetch";
 // Removed logo since we don't need it anymore
 import './App.css';
 import './components/TimeTable'
 import TimeTable from './components/TimeTable';
 import SlotTable from './components/slotTable';
-class App extends Component {
-	constructor(state) {
-		super(state);
-		this.state = {
-			data: {},
-			error: null
-		};
+class App extends React.Component {
+	
+	state={
+		array:[{code:"Course Code",key:1,code2:"Course Title"}],i:0
 	}
+	
+	
+	addCourse=(course)=>{
+		course.code2="undefined";
+		let a=[...this.state.array,course];
+			this.setState({
+				array: a  
+		});
+	}
+	
+	
+//	constructor(state) {
+//		super(state);
+//		this.state = {
+//			data: {},
+//			error: null
+//		};
+//	}
 
 	// async componentWillMount() {
 	// 	var res = await fetch("/account");
@@ -26,14 +50,12 @@ class App extends Component {
 
 	render() {
 		return (
-			<div className="container">
-				<h1>Hello World!</h1>
-				<p>I just created my first React App</p>
-				
-				<SlotTable/>
-				<TimeTable/>
-				
-
+			<div>
+			<Search addCourse={this.addCourse}/>
+			<Course array={this.state.array}/>
+			<SlotTable/>
+			<TimeTable/>
+			<CourseTable/>   
 			</div>
 		);
 	}

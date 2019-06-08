2d943742742899f00b78b9a729c9f21a3e6557be
Namit Nathwani <namsnath@gmail.com>
2019-06-08 13:38:17 +0530

    Integrated heatmap route with slots and cleaned up code

--- client/src/components/slotTable.js
+++ client/src/components/slotTable.js
@@ -3,94 +3,93 @@ import axios from 'axios';
 import './slotTable.css';
 // import Icon from 'react-native-vector-icons/FontAwesome';
 class SlotTable extends Component {
-    state={
-        posts:[]
-        }
-componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/users')
-       .then(res=>{
-            this.setState({
-                posts:res.data  
-            })
-        })
-    }
+	state = {
+		heatmap: []
+	}
+	componentWillMount() {
+		axios.get('/course/getFullHeatmap')
+			.then(res => this.setState({ heatmap: res.data.data }))
+	}
 
 
-handleaddedfilters=(e)=>{
-    var x=document.getElementById("selectedFilter");
-    console.log(x);
-    x.removeChild(e.target);
- }
-handleclick=(e)=>{
-                var x=document.getElementById("selectedFilter");
-                var y=document.createElement("a");
-                x.appendChild(y);
-                y.textContent=e.target.textContent;
-                y.href="#";
-                y.setAttribute("onClick","{this.handleaddedfilters}");
-            }
+	handleAddedFilters = (e) => {
+		var x = document.getElementById("selectedFilter");
+		console.log(x);
+		x.removeChild(e.target);
+	}
+
+	handleclick = (e) => {
+		var x = document.getElementById("selectedFilter");
+		var y = document.createElement("a");
+		x.appendChild(y);
+		y.textContent = e.target.textContent;
+		y.href = "#";
+		y.setAttribute("onClick", "{this.handleAddedFilters}");
+	}
 
 
-handleClick=(e)=>{
-    var data=document.getElementById("data");
-    if(data.className==="hide"){
-        data.className="show";  
-    }
-    else{
-        data.className="hide";
-    }
-}
+	handleClick = (e) => {
+		var data = document.getElementById("data");
+		if (data.className === "hide") {
+			data.className = "show";
+		}
+		else {
+			data.className = "hide";
+		}
+	}
 
-handleCross=(e)=>{
-    var data=document.getElementById("data");
-    data.className="hide";  
-}
-handlevenues=(e)=>{
-    var x=document.getElementById("selectedFilter");
-    var y=document.createElement("a");
-    x.appendChild(y);
-    y.textContent=e.target.textContent;
-    y.href="#";
-}
-    render(){
-        var courses=this.state.posts.map(value=>{
-            return(
-                <div className="slots" key={value.id}>
-                        <h4>Name:{value.name}</h4>
-                        <h5>Email:{value.email}</h5>
-                        <p>Website:{value.website}</p>
-                </div>
-            )
-        })
-        return(
-         
-            <div className="righttable">
-                <div className="tiles">
-                <button onClick={this.handleclick} className="notclicked">Lab</button>
-                <button onClick={this.handleclick} className="notclicked">Theory </button>
-                <button onClick={this.handleclick} className="notclicked">Project</button>
-                <a href="#" id="venuefilter" onClick={this.handleClick}><span>Filter by Venue</span><i class="fas fa-filter" id="filter"></i></a>
-                <div className="hide" id="data">
-                    <div className="venues">Venue Filters</div>
-                    <a href="#" onClick={this.handlevenues}><span>SJT</span></a>
-                    <a href="#" onClick={this.handlevenues}><span>TT</span></a>
-                    <a href="#" onClick={this.handlevenues}><span>GDN</span></a>
-                    <a href="#" onClick={this.handlevenues}><span>MB</span></a>
-                    <a href="#" onClick={this.handlevenues}><span>SMV</span></a>
-                    <a href="#" onClick={this.handleCross}><i class="material-icons" id="cross" >
-                            clear
+	handleCross = (e) => {
+		var data = document.getElementById("data");
+		data.className = "hide";
+	}
+
+	handleVenues = (e) => {
+		var x = document.getElementById("selectedFilter");
+		var y = document.createElement("a");
+		x.appendChild(y);
+		y.textContent = e.target.textContent;
+		y.href = "#";
+	}
+
+	render() {
+		var courses = this.state.heatmap.map(value => {
+			return (
+				<div className="slots" key={value._id}>
+					<h4>{value.slot}</h4>
+					<h5>{value.faculty}</h5>
+					<p>{value.venue} - {value.course_type}</p>
+				</div>
+			)
+		})
+		return (
+
+			<div className="righttable">
+				<div className="tiles">
+					<button onClick={this.handleclick} className="notclicked">Lab</button>
+					<button onClick={this.handleclick} className="notclicked">Theory </button>
+					<button onClick={this.handleclick} className="notclicked">Project</button>
+					<a href="#" id="venuefilter" onClick={this.handleClick}><span>Filter by Venue</span><i class="fas fa-filter" id="filter"></i></a>
+					<div className="hide" id="data">
+						<div className="venues">Venue Filters</div>
+						<a href="#" onClick={this.handleVenues}><span>SJT</span></a>
+						<a href="#" onClick={this.handleVenues}><span>TT</span></a>
+						<a href="#" onClick={this.handleVenues}><span>GDN</span></a>
+						<a href="#" onClick={this.handleVenues}><span>MB</span></a>
+						<a href="#" onClick={this.handleVenues}><span>SMV</span></a>
+						<a href="#" onClick={this.handleCross}><i class="material-icons" id="cross" >
+							clear
                     </i></a>
-                </div>
-                <hr></hr>
-                <div className="filtersAdded" id="selectedFilter">Filters Added:</div>
-                <hr></hr>
-                </div>
+					</div>
+					<hr></hr>
+					<div className="filtersAdded" id="selectedFilter">Filters Added:</div>
+					<hr></hr>
+				</div>
 
-                {courses}
+				{courses}
 
-            </div>
-           
-        )
-    }
+			</div>
+
+		)
+	}
 }
 export default SlotTable;

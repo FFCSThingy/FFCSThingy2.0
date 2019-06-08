1b4f0ed04ad53d19804fffc296bc169093f3ee6c
Namit Nathwani <namsnath@gmail.com>
2019-06-08 17:31:16 +0530

    Updated heatmap to include timestamp-based fetching

--- backend/routes/course.js
+++ backend/routes/course.js
@@ -11,12 +11,17 @@ const router = express.Router();
 router.use(express.json({ limit: '50mb' }));
 router.use(express.urlencoded({ limit: '50mb', extended: false }));
 
-router.get('/getFullHeatMap', async (req, res, next) => {
-	console.log(req.body.timestamp);
-	if(!req.body.timestamp)	// TODO: Add timestamp comparison here 
+router.get('/getFullHeatmap/:timestamp?', async (req, res, next) => {
+	if (!req.params.timestamp)
+		return res.json({ success: true, data: await course.getFullHeatmap() });
+
+	var reqTimestamp = new Date(req.params.timestamp)
+	var systemTimestamp = new Date(await system.getRepopulateTime());
+
+	if (reqTimestamp < systemTimestamp)
 		res.json({ success: true, data: await course.getFullHeatmap() });
 	else
-		res.status(304).json({ success: true, message: "Up To Date" });	
+		res.status(304).json({ success: true, message: "Up To Date" });
 });
 
 router.get('/getCourseList', async (req, res, next) => {

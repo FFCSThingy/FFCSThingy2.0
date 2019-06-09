7ec65c071500005cc788b67fde2c17ad7d6ca65f
ffardeen <fardeenasif.khan2018@vitstudent.ac.in>
2019-06-09 16:35:09 +0530

    Updates to search and course table Course-table is now css grid and responsive

--- client/src/components/course-select-table.js
+++ client/src/components/course-select-table.js
@@ -33,25 +33,50 @@ class CourseSelect extends React.Component {
 	render() {
 		var courses = this.state.courseList.map(value => {
 			return (
-				<tr className="courses" key={value.code} onClick={() => this.props.selectCourse(value.code)}>
-					<td className="course-code">{value.code}</td>
-					<td className="course-title">{value.title}</td>
-					<td className="course-title">{value.credits}</td>
-				</tr>
+				// <tr className="courses" key={value.code} onClick={() => this.props.selectCourse(value.code)}>
+				// 	<td className="course-code">{value.code}</td>
+				// 	<td className="course-title">{value.title}</td>
+				// 	<td className="course-title">{value.credits}</td>
+				// </tr>
+
+			<div className="courses" key={value.code} onClick={() => this.props.selectCourse(value.code)}>
+				<div class="container-left">
+					<h1 className="course-title">{value.title}</h1>
+				</div>
+				<div class="container-right">
+					<ul>
+						<li className="course-code">{value.code}</li>
+						<li className="credits">{value.credits}</li>
+					</ul>
+				</div>
+			</div>
 
 			)
 		})
 
 		return (
-			<div className="courselist">
-				<table className="courseTable" >
-					<tr>
-						<th className="code-head">Course Code</th>
-						<th className="title-head">Course Title</th>
-						<th className="title-head">Credits</th>
-					</tr>
-					{courses}
-				</table>
+			// <div className="courselist">
+			// 	<table className="courseTable" >
+			// 		<tr>
+			// 			<th className="code-head">Course Code</th>
+			// 			<th className="title-head">Course Title</th>
+			// 			<th className="title-head">Credits</th>
+			// 		</tr>
+			// 		{courses}
+			// 	</table>
+			// </div>
+			<div className="courseTable">
+				<div className="table-head">
+					<div className="container-left">
+						<h1 className="title-head">Course Title</h1>
+					</div><div className="container-right-head">
+						<ul>
+							<li className="code-head">Code</li>
+							<li className="credits-head">Credits</li>
+						</ul>
+					</div>
+				</div>
+				{courses}
 			</div>
 		);
 	}

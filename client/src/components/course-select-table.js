ce7584e2bc4159a8081b23d61214aff89291efd0
Namit Nathwani <namsnath@gmail.com>
2019-06-12 16:21:11 +0530

    Added navigation for courses and curriculum change

--- client/src/components/course-select-table.js
+++ client/src/components/course-select-table.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Card, CardColumns, Form } from 'react-bootstrap';
-import '../css/course-select-table.css';
 import axios from 'axios';
+import React from 'react';
+import { Card, CardColumns, Col, Container, Form, Row, Nav } from 'react-bootstrap';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../css/course-select-table.css';
 // import Search from './searchBar';
 
 class CourseSelect extends React.Component {
@@ -39,7 +39,7 @@ class CourseSelect extends React.Component {
 		axios.get("/curriculum/getPrefixes")
 			.then(res => {
 				if (res.data.success) {
-					this.setState({ curriculumList: res.data.data, selectedCurriculum: '17BCI' });
+					this.setState({ curriculumList: res.data.data, selectedCurriculum: '17BCE' });
 				} else
 					this.setState({ error: res.data.message })
 			});
@@ -51,7 +51,7 @@ class CourseSelect extends React.Component {
 					localStorage.setItem('17BCE', JSON.stringify(res.data.data));
 				} else
 					this.setState({ error: res.data.message })
-			});	
+			});
 	}
 
 	handleChange(event) {
@@ -67,16 +67,16 @@ class CourseSelect extends React.Component {
 		var courseList = JSON.parse(localStorage.getItem('courseList'));
 		var courseSlots = this.props.heatmap.reduce((a, v) => {
 			var slots = v.slot.split('+');
-			
-			if (v.slot === 'NIL') ;
-			else if (a[v.code]) 
+
+			if (v.slot === 'NIL');
+			else if (a[v.code])
 				a[v.code].slots = a[v.code].slots.concat(slots);
 			else a[v.code] = { slots: slots, title: courseList.filter(c => c.code === v.code)[0].title };
 
 			return a;
 		}, {});
 		courseSlots = Object.keys(courseSlots)
-			.reduce((a, v) => [...a, {code: v, slots: Array.from(new Set(courseSlots[v].slots)), title: courseSlots[v].title }], []);
+			.reduce((a, v) => [...a, { code: v, slots: Array.from(new Set(courseSlots[v].slots)), title: courseSlots[v].title }], []);
 		return courseSlots;
 		// this.setState({ courseSlotList: courseSlots });
 	}
@@ -86,19 +86,19 @@ class CourseSelect extends React.Component {
 		axios.get("/curriculum/getCurriculumFromPrefix/" + prefix)
 			.then(res => {
 				if (res.data.success) {
-					this.setState({ curriculum: res.data.data });
+					this.setState({ curriculum: res.data.data, selectedCurriculum: prefix });
 					localStorage.setItem(prefix, JSON.stringify(res.data.data));
 				} else
 					this.setState({ error: res.data.message })
-			});	
+			});
 	}
 
 	doSearch() {
 		var searchString = this.state.searchString.toUpperCase();
-		
-		if(searchString === '') return this.state.courseList;
-		if(searchString.endsWith('+')) searchString = searchString.substring(0, searchString.length -1)
-		
+
+		if (searchString === '') return this.state.courseList;
+		if (searchString.endsWith('+')) searchString = searchString.substring(0, searchString.length - 1)
+
 		var searchBySlots = true;
 		var slots_title = new Set(["A1", "A2", "B1", "B2", "C1", "C2", "D1", "D2", "E1", "E2", "F1", "F2", "G1", "G2", "L1", "L10", "L11", "L12", "L13", "L14", "L15", "L16", "L19", "L2", "L20", "L21", "L22", "L23", "L24", "L25", "L26", "L27", "L28", "L29", "L3", "L30", "L31", "L32", "L33", "L34", "L35", "L36", "L37", "L38", "L39", "L4", "L40", "L41", "L42", "L43", "L44", "L45", "L46", "L47", "L48", "L49", "L5", "L50", "L51", "L52", "L53", "L54", "L55", "L56", "L57", "L58", "L59", "L6", "L60", "L7", "L71", "L72", "L73", "L74", "L75", "L76", "L77", "L78", "L79", "L8", "L80", "L81", "L82", "L83", "L84", "L85", "L86", "L87", "L88", "L89", "L9", "L90", "L91", "L92", "L93", "L94", "TA1", "TA2", "TAA1", "TAA2", "TB1", "TB2", "TBB2", "TC1", "TC2", "TCC1", "TCC2", "TD1", "TD2", "TDD2", "TE1", "TE2", "TF1", "TF2", "TG1", "TG2", "V1", "V10", "V11", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "W2", "X1", "X2", "Y1", "Y2", "Z", ""]);
 
@@ -113,7 +113,7 @@ class CourseSelect extends React.Component {
 		var searchBySlots = searchStringSlots.reduce((a, v) => (a && slots_title.has(v)), searchBySlots);
 
 		var filteredCodes = [];
-		if(searchBySlots) {
+		if (searchBySlots) {
 			filteredCodes = this.props.heatmap.filter(v => {
 				return searchStringSlots.every(c => v.slot.split('+').includes(c));
 			}).map(v => v.code);
@@ -142,7 +142,7 @@ class CourseSelect extends React.Component {
 						<Form.Control
 							as='select'
 							name='selectedCurriculum'
-							value={this.state.selectedCurriculum}
+							// value={this.state.selectedCurriculum}
 							onChange={this.handleChange.bind(this)}
 						>
 							{curriculumChoices}
@@ -157,6 +157,30 @@ class CourseSelect extends React.Component {
 							onChange={this.handleChange.bind(this)}
 						/>
 					</Col>
+				</Row>
+
+				<Row style={{'padding-top': '1vh'}}>
+					<Nav 
+						variant="tabs" 
+						defaultActiveKey='ALL'
+						onSelect={selected => this.setState({ selectedCategory: selected })}
+					>
+						<Nav.Item>
+							<Nav.Link eventKey='ALL'>ALL</Nav.Link>
+						</Nav.Item>
+						<Nav.Item>
+							<Nav.Link eventKey='PC'>PC</Nav.Link>
+						</Nav.Item>
+						<Nav.Item>
+							<Nav.Link eventKey='UC'>UC</Nav.Link>
+						</Nav.Item>
+						<Nav.Item>
+							<Nav.Link eventKey='PE'>PE</Nav.Link>
+						</Nav.Item>
+						<Nav.Item>
+							<Nav.Link eventKey='UE'>UE</Nav.Link>
+						</Nav.Item>
+					</Nav>
 				</Row>
 			</Container>
 		)
@@ -179,20 +203,22 @@ class CourseSelect extends React.Component {
 	}
 
 	renderCourses() {
-		var list =  this.doSearch();
-		
+		var list = this.doSearch();
+
 		if (this.state.selectedCategory !== 'ALL' && Object.keys(this.state.curriculum).length) {
 			var cat = this.state.selectedCategory.toLowerCase();
 			var currCourses = this.state.curriculum[cat].map(c => c.code);
-		
+
 			list = list.filter(v => {
 				return currCourses.includes(v.code);
 			});
-		}	
+		}
+
+		if(list.length === 0) this.renderNormalCard({code: '', title: '', credits: ''});
 
 		return list.map(value => {
-				return this.renderNormalCard(value)
-			});
+			return this.renderNormalCard(value)
+		});
 	}
 
 	render() {

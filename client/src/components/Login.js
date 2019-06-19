61e2d01e567da044883adbff161935215175e27d
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-06-20 01:30:12 +0530

    Mobile view fixed - login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../css/login.css'
-import { Container, Row, Card, CardColumns } from 'react-bootstrap';
+import { Container, Row, Card } from 'react-bootstrap';
 import featuresImage from "../images/ffcsfeatures.png";
+import { FaHeart } from 'react-icons/fa'
 
 class Login extends React.Component {
 
@@ -11,64 +12,46 @@ class Login extends React.Component {
 		return (
 			<Container className="loginContainer">
 				<Row className="logoContainer">
-					FFCS Thingy
+					<span className="ffcsthingy">FFCSThingy</span>
 				</Row>
 
-				<Row className="loginCard">
-				<Card>
-				<CardColumns>
-					
-					<Card>
+				<Row className="cards">
+
+					<Card className="firstCard">
 						<Card.Body>
-						<img className="features" src={featuresImage} alt="" />
-						{/* <Card.Text className="featureList">
-							Plan and create your timetable, ahead of FFCS. 
-						</Card.Text>
-						<Card.Text className="featureList">
-							Select your courses, and filter your slots with ease.
-						</Card.Text>
-						<Card.Text className="featureList">
-							See which slots clash beforehand. 
-						</Card.Text>
-						<Card.Text className="featureList"> 
-							Create multiple timetables. 
-						</Card.Text> */}
+						<img className="features" src={featuresImage} alt="" width="300px"/>
 						</Card.Body>
 					</Card>
-					<Card className="cardTwo">
-							<Card.Body>
-							<Card.Title className="titleHead">A smarter way to<br></br> FFCS</Card.Title>
-							{/* <Card.Title className="signInText">Sign in</Card.Title> */}
-								{/* <Container className="break"></Container> */}
-								<Container className="loginButtonContainer">
-									<a href={process.env.REACT_APP_BASE_URL+'/auth/google'}>
-									<button href={process.env.REACT_APP_BASE_URL+'/auth/google'} type="button" class="google-button">
-									<span class="google-button__icon">
-										<svg viewBox="0 0 366 372" xmlns="http://www.w3.org/2000/svg"><path d="M125.9 10.2c40.2-13.9 85.3-13.6 125.3 1.1 22.2 8.2 42.5 21 59.9 37.1-5.8 6.3-12.1 12.2-18.1 18.3l-34.2 34.2c-11.3-10.8-25.1-19-40.1-23.6-17.6-5.3-36.6-6.1-54.6-2.2-21 4.5-40.5 15.5-55.6 30.9-12.2 12.3-21.4 27.5-27 43.9-20.3-15.8-40.6-31.5-61-47.3 21.5-43 60.1-76.9 105.4-92.4z" id="Shape" fill="#EA4335"/><path d="M20.6 102.4c20.3 15.8 40.6 31.5 61 47.3-8 23.3-8 49.2 0 72.4-20.3 15.8-40.6 31.6-60.9 47.3C1.9 232.7-3.8 189.6 4.4 149.2c3.3-16.2 8.7-32 16.2-46.8z" id="Shape" fill="#FBBC05"/><path d="M361.7 151.1c5.8 32.7 4.5 66.8-4.7 98.8-8.5 29.3-24.6 56.5-47.1 77.2l-59.1-45.9c19.5-13.1 33.3-34.3 37.2-57.5H186.6c.1-24.2.1-48.4.1-72.6h175z" id="Shape" fill="#4285F4"/><path d="M81.4 222.2c7.8 22.9 22.8 43.2 42.6 57.1 12.4 8.7 26.6 14.9 41.4 17.9 14.6 3 29.7 2.6 44.4.1 14.6-2.6 28.7-7.9 41-16.2l59.1 45.9c-21.3 19.7-48 33.1-76.2 39.6-31.2 7.1-64.2 7.3-95.2-1-24.6-6.5-47.7-18.2-67.6-34.1-20.9-16.6-38.3-38-50.4-62 20.3-15.7 40.6-31.5 60.9-47.3z" fill="#34A853"/></svg>
-									</span>
-									<span class="google-button__text">Sign in with Google</span>
-									</button>
-									</a>
-								</Container>
-							</Card.Body>
-						</Card>
-				</CardColumns>
-				</Card>
-					{/* <h1>Sign in</h1>
-					<div className="underline" align='center'></div>
-					<h4>A smarter way to FFCS</h4> */}
+					<Card className="secondCard">
+						<Card.Body>
+							<Card.Title className="titleHead">A smarter way to<br/>FFCS</Card.Title>
+								<a href={process.env.REACT_APP_BASE_URL+'/auth/google'}>
+								<button href={process.env.REACT_APP_BASE_URL+'/auth/google'} type="button" class="google-button">
+								<span class="google-button__icon">
+									<svg viewBox="0 0 366 372" xmlns="http://www.w3.org/2000/svg"><path d="M125.9 10.2c40.2-13.9 85.3-13.6 125.3 1.1 22.2 8.2 42.5 21 59.9 37.1-5.8 6.3-12.1 12.2-18.1 18.3l-34.2 34.2c-11.3-10.8-25.1-19-40.1-23.6-17.6-5.3-36.6-6.1-54.6-2.2-21 4.5-40.5 15.5-55.6 30.9-12.2 12.3-21.4 27.5-27 43.9-20.3-15.8-40.6-31.5-61-47.3 21.5-43 60.1-76.9 105.4-92.4z" id="Shape" fill="#EA4335"/><path d="M20.6 102.4c20.3 15.8 40.6 31.5 61 47.3-8 23.3-8 49.2 0 72.4-20.3 15.8-40.6 31.6-60.9 47.3C1.9 232.7-3.8 189.6 4.4 149.2c3.3-16.2 8.7-32 16.2-46.8z" id="Shape" fill="#FBBC05"/><path d="M361.7 151.1c5.8 32.7 4.5 66.8-4.7 98.8-8.5 29.3-24.6 56.5-47.1 77.2l-59.1-45.9c19.5-13.1 33.3-34.3 37.2-57.5H186.6c.1-24.2.1-48.4.1-72.6h175z" id="Shape" fill="#4285F4"/><path d="M81.4 222.2c7.8 22.9 22.8 43.2 42.6 57.1 12.4 8.7 26.6 14.9 41.4 17.9 14.6 3 29.7 2.6 44.4.1 14.6-2.6 28.7-7.9 41-16.2l59.1 45.9c-21.3 19.7-48 33.1-76.2 39.6-31.2 7.1-64.2 7.3-95.2-1-24.6-6.5-47.7-18.2-67.6-34.1-20.9-16.6-38.3-38-50.4-62 20.3-15.7 40.6-31.5 60.9-47.3z" fill="#34A853"/></svg>
+								</span>
+								<span class="google-button__text">Sign in with Google</span>
+								</button>
+								</a>
+						</Card.Body>
+					</Card>
 				</Row>
-				{/* <Row className="featureContainer">
-					<h5>Plan and create your timetable, ahead of FFCS</h5>
-					<h5>Select your courses, and filter your slots</h5>
-					<h5>See which slots clash beforehand</h5>
-					<h5>Create multiple timetables</h5>
-				</Row> */}
-				<Row className="footerTextContainer">
-					Lorem ipsum dolor sheep
+				<Row>
+					<Card className="thirdCard">
+						<Card.Body>
+							<ol>
+								<li> Login and choose your curriculum.</li>
+								<li> Search for courses with slot or title</li>
+								<li> Filter slots by venue and type, then add them to your timetable.</li>
+							</ol>
+						</Card.Body>
+					</Card>
 				</Row>
-				
-			</Container>	
+				<Row className="footer">
+					{/* <span className="footerText">Made with <FaHeart /> by CSI-VIT</span> */}
+				</Row>
+
+			</Container>
 		)
 	}
 }

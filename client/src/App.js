74e403cfc1a93622091edf28848453d184c29c67
Namit Nathwani <namsnath@gmail.com>
2019-06-25 23:23:02 +0530

    Timetable removal fix

--- client/src/App.js
+++ client/src/App.js
@@ -910,7 +910,7 @@ class App extends React.Component {
 	}
 
 	unselectSlots = (course) => {
-		course.timetableName = this.state.activeTimetable
+		// course.timetableName = this.state.activeTimetable
 		if (course.slot !== 'NIL') {
 			course.slot.split('+').map(v => this.setState(prevState => {
 				let clashMap = { ...prevState.clashMap };
@@ -920,7 +920,7 @@ class App extends React.Component {
 		}
 
 		this.setState(prevState => {
-			let timetable = prevState.timetable.filter(v => !(course.code === v.code && course.faculty === v.faculty && course.slot === v.slot && course.venue === v.venue && course.timetableName === prevState.activeTimetable));
+			let timetable = prevState.timetable.filter(v => !(course.code === v.code && course.faculty === v.faculty && course.slot === v.slot && course.venue === v.venue && v.timetableName === prevState.activeTimetable));
 
 			this.doSetSelectedCourses(timetable);
 			return { timetable }

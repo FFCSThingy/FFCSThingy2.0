a85feb3a874e1b9985cb5d08f2244118d98fab12
Namit Nathwani <namsnath@gmail.com>
2019-06-26 15:42:55 +0530

    Minor fix. Might fix nextTick related errors

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const GOOGLE_CLIENT_SECRET = "UUNrcLqOXmnP_HweyJirA9VA";
 
 
 passport.serializeUser(function (user, done) {
-	done(null, user);
+	return done(null, user);
 });
 
 passport.deserializeUser(async function (doc, done) {
@@ -133,8 +133,10 @@ app.use(logger('dev'));
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+	
 	if (req.method === 'OPTIONS') { return res.status(200).end(); }
+	
 	next();
 });
 
@@ -154,7 +156,7 @@ app.get('/account', ensureAuthenticated, function (req, res) {
 		vtopSignedIn: req.user.vtopSignedIn,
 	}
 	// res.json({ success: true, data: data });
-	res.json(data);
+	return res.json(data);
 });
 
 

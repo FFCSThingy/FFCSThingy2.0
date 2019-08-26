927b7786959e9f66c1b37441292ae53941407896
Namit Nathwani <namsnath@gmail.com>
2019-08-26 23:12:10 +0530

    Change theme to a constant with its own file

--- client/src/App.js
+++ client/src/App.js
@@ -14,8 +14,9 @@ import CourseTable from './components/SelectedCoursesTable';
 import SelectTimeTable from './components/TimetableSwitcher';
 import CustomNavbar from './components/CustomNavbar';
 
+// Constants
+import { THEMES as THEMES } from './constants/Themes';
 
-import "whatwg-fetch";
 import './App.css';
 import './css/CustomNavbar.css'
 
@@ -54,169 +55,6 @@ class App extends React.Component {
 			selectedCurriculum: localStorage.getItem('selectedCurriculum') || 'Curriculum',
 
 			activeTheme: localStorage.getItem('theme') || 'default',
-			themes: {
-				default: {
-					name: 'Default',
-					properties: {
-						'brand-color': '#7c87e8',
-						'body-background-color': '#C3D1F5',
-						'select-table-background-color': '#f6f6f6',
-						'card-background-color': '#ffffff',
-						'card-highlight-color': '#ECEBFE',
-						'button-highlight-color': '#36e2a8',
-						'timetable-theory-color': '#FFDCB5',
-						'timetable-lab-color': '#99D1FF',
-						'timetable-col1-color': '#e5e5e5',
-						'text-color-default': '#ffffff',
-						'text-color-inverted': '#000000',
-						'text-color-other': '#666666',
-						'card-clash-text-color': '#ff6961',
-						'card-selected-text-color': '#00b200',
-						'dropdown-border-color': '#4c56b2',
-						'dropshadow-default': '#00000033',
-						'dropshadow-highlight': '#00000059',
-						'alternate-table-item-color': '#ECEBFE',
-					}
-				},
-				dark: {
-					name: 'Eerie Black',
-					properties: {
-						'brand-color': '#121317',
-						'body-background-color': '#15181f',
-						'select-table-background-color': '#1f2229',
-						'card-background-color': '#27282C',
-						'card-highlight-color': '#4993D0',
-						'button-highlight-color': '#36e2a8',
-						'timetable-theory-color': '#6B4896',
-						'timetable-lab-color': '#008C7E',
-						'timetable-col1-color': '#121317',
-						'text-color-default': '#ffffff',
-						'text-color-inverted': '#ffffff',
-						'text-color-other': '#ffffff',
-						'card-clash-text-color': '#ff6961',
-						'card-selected-text-color': '#ffffff',
-						'dropdown-border-color': '#1f2229',
-						'dropshadow-default': '#00000033',
-						'dropshadow-highlight': '#000000',
-						'alternate-table-item-color': '#1f2229',
-					}
-				},
-				yellowish: {
-					name: 'Mellow Yellow',
-					properties: {
-						'brand-color': '#FFDA76',
-						'body-background-color': '#FFECC5',
-						'select-table-background-color': '#f6f6f6',
-						'card-background-color': '#ffffff',
-						'card-highlight-color': '#FFFAE9',
-						'button-highlight-color': '#36e2a8',
-						'timetable-theory-color': '#FFDCB5',
-						'timetable-lab-color': '#99D1FF',
-						'timetable-col1-color': '#e5e5e5',
-						'text-color-default': '#2F4F4F',
-						'text-color-inverted': '#2F4F4F',
-						'text-color-other': '#666666',
-						'card-clash-text-color': '#ff6961',
-						'card-selected-text-color': '#00b200',
-						'dropdown-border-color': '#FFD156',
-						'dropshadow-default': '#00000033',
-						'dropshadow-highlight': '#00000059',
-						'alternate-table-item-color': '#FFFAE9',
-					}
-				},
-				coral: {
-					name: 'Light Coral',
-					properties: {
-						'brand-color': '#E87A85',
-						'body-background-color': '#F4C3D1',
-						'select-table-background-color': '#f6f6f6',
-						'card-background-color': '#ffffff',
-						'card-highlight-color': '#FFEDED',
-						'button-highlight-color': '#36e2a8',
-						'timetable-theory-color': '#FFDCB5',
-						'timetable-lab-color': '#99D1FF',
-						'timetable-col1-color': '#e5e5e5',
-						'text-color-default': '#ffffff',
-						'text-color-inverted': '#000000',
-						'text-color-other': '#666666',
-						'card-clash-text-color': '#ff6961',
-						'card-selected-text-color': '#00b200',
-						'dropdown-border-color': '#B24C56',
-						'dropshadow-default': '#00000033',
-						'dropshadow-highlight': '#00000059',
-						'alternate-table-item-color': '#FFEDED',
-					}
-				},
-				green: {
-					name: 'Pretty Green',
-					properties: {
-						'brand-color': '#7AE88B',
-						'body-background-color': '#C6F4C3',
-						'select-table-background-color': '#f6f6f6',
-						'card-background-color': '#ffffff',
-						'card-highlight-color': '#EDFFF2',
-						'button-highlight-color': '#f4c2c6',
-						'timetable-theory-color': '#FFDCB5',
-						'timetable-lab-color': '#99D1FF',
-						'timetable-col1-color': '#e5e5e5',
-						'text-color-default': '#2F4F4F',
-						'text-color-inverted': '#000000',
-						'text-color-other': '#666666',
-						'card-clash-text-color': '#ff6961',
-						'card-selected-text-color': '#00b200',
-						'dropdown-border-color': '#4CB25C',
-						'dropshadow-default': '#00000033',
-						'dropshadow-highlight': '#00000059',
-						'alternate-table-item-color': '#EDFFF2',
-					}
-				},
-				blue: {
-					name: 'Devspace Light',
-					properties: {
-						'brand-color': '#7AD7E8',
-						'body-background-color': '#C3F4F2',
-						'select-table-background-color': '#f6f6f6',
-						'card-background-color': '#ffffff',
-						'card-highlight-color': '#EDF9FF',
-						'button-highlight-color': '#36e2a8',
-						'timetable-theory-color': '#FFDCB5',
-						'timetable-lab-color': '#99D1FF',
-						'timetable-col1-color': '#e5e5e5',
-						'text-color-default': '#2F4F4F',
-						'text-color-inverted': '#000000',
-						'text-color-other': '#666666',
-						'card-clash-text-color': '#ff6961',
-						'card-selected-text-color': '#00b200',
-						'dropdown-border-color': '#1d98ae',
-						'dropshadow-default': '#00000033',
-						'dropshadow-highlight': '#00000059',
-						'alternate-table-item-color': '#EDF9FF',
-					}
-				},
-				pink: {
-					name: 'Persian Pink',
-					properties: {
-						'brand-color': '#E87ABC',
-						'body-background-color': '#F4C3E9',
-						'select-table-background-color': '#f6f6f6',
-						'card-background-color': '#ffffff',
-						'card-highlight-color': '#FFEDF4',
-						'button-highlight-color': '#36e2a8',
-						'timetable-theory-color': '#FFDCB5',
-						'timetable-lab-color': '#99D1FF',
-						'timetable-col1-color': '#e5e5e5',
-						'text-color-default': '#ffffff',
-						'text-color-inverted': '#000000',
-						'text-color-other': '#666666',
-						'card-clash-text-color': '#ff6961',
-						'card-selected-text-color': '#00b200',
-						'dropdown-border-color': '#B24C89',
-						'dropshadow-default': '#00000033',
-						'dropshadow-highlight': '#00000059',
-						'alternate-table-item-color': '#FFEDF4',
-					}
-				}
-			},
 
 			clashMap: {
 				A1: {
@@ -1025,7 +863,7 @@ class App extends React.Component {
 	}
 
 	updateTheme = () => {
-		var theme = this.state.themes[this.state.activeTheme];
+		var theme = THEMES[this.state.activeTheme];
 		localStorage.setItem('theme', this.state.activeTheme);
 		Object.keys(theme.properties).map(v =>
 			document.documentElement.style.setProperty(`--${v}`, theme.properties[v])
@@ -1081,7 +919,7 @@ class App extends React.Component {
 					<CustomNavbar
 						user={this.state.user}
 						creditCount={this.getCreditCount()}
-						themes={this.state.themes}
+						themes={THEMES}
 						curriculumList={this.state.curriculumList}
 						selectedCurriculum={this.state.selectedCurriculum}
 

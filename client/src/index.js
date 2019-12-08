4f695a795998fda3be5a88862e44ceeb3f4bf93c
Namit Nathwani <namsnath@gmail.com>
2019-12-08 22:34:14 +0530

    update: Added PrivateRoute placeholder for future use

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Route, BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+// import API from './API';
 
 import "./index.css";
 import App from "./App";
 import Login from './components/Login';
+
 import * as serviceWorker from "./serviceWorker";
+
+// async function isAuthenticated(){
+// 	return await API.get("/account")
+// 		.then(res => true)
+// 		.catch(err => false);	
+// }
 
 const routing = (
 	<Router>
 		<div>
-			<Route exact path='/' component={Login} />
-			<Route path='/dashboard' component={App} />
+			<Switch>
+				<Route exact path='/' component={Login} />
+				<Route path='/dashboard' component={App} />
+				{/* <PrivateRoute 
+					path='/dashboard' 
+					component={App} 
+					isAuthenticated={!!isAuthenticated()}
+				/> */}
+			</Switch>
 		</div>
 	</Router>
 )

441ab38a2cb8eb7b119bee4f565b67ed5becb918
Namit Nathwani <namsnath@gmail.com>
2020-04-13 18:24:46 +0530

    feat: Ports App to TS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,8 +8,8 @@ import ReactGA from 'react-ga';
 import API from './API';
 
 import PrivateRoute from './components/PrivateRoute';
-import Login from './components/Login.tsx';
-import Dashboard from './components/Dashboard.tsx';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 ReactGA.initialize('UA-156974674-1');
 ReactGA.pageview(window.location.pathname + window.location.search);

2aaa98ad20505c6d61f605ca505368779465e3df
Namit Nathwani <namsnath@gmail.com>
2020-06-17 15:07:45 +0530

    update: New timetable data
    - Changes Clashmap
    - Changes timetable constants

--- client/src/constants/ClashMap.ts
+++ client/src/constants/ClashMap.ts
@@ -2,509 +2,651 @@ import Clashmap from '../models/constants/Clashmap';
 
 const CLASHMAP: Clashmap = {
 	A1: {
-		clashesWith: ['A1', 'L1', 'L2', 'L13', 'L14'],
+		clashesWith: ['A1', 'L1', 'L9'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	B1: {
-		clashesWith: ['B1', 'L7', 'L8', 'L19', 'L20'],
+		clashesWith: ['B1', 'L2', 'L13'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	C1: {
-		clashesWith: ['C1', 'L13', 'L14', 'L25', 'L26'],
+		clashesWith: ['C1', 'L3', 'L14'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	D1: {
-		clashesWith: ['D1', 'L19', 'L20', 'L3', 'L4'],
+		clashesWith: ['D1', 'L7', 'L15'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	E1: {
-		clashesWith: ['E1', 'L25', 'L26', 'L9', 'L10'],
-		isFilled: false,
-		currentlyClashesWith: [],
-	},
-	F1: {
-		clashesWith: ['F1', 'L1', 'L2', 'L15', 'L16'],
-		isFilled: false,
-		currentlyClashesWith: [],
-	},
-	G1: {
-		clashesWith: ['G1', 'L7', 'L8', 'L21', 'L22'],
+		clashesWith: ['E1', 'L8', 'L19'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	TA1: {
-		clashesWith: ['TA1', 'L27', 'L28'],
+		clashesWith: ['TA1', 'L20'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	TB1: {
-		clashesWith: ['TB1', 'L4', 'L5'],
+		clashesWith: ['TB1', 'L21'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	TC1: {
-		clashesWith: ['TC1', 'L10', 'L11'],
-		isFilled: false,
-		currentlyClashesWith: [],
-	},
-	TE1: {
-		clashesWith: ['TE1', 'L22', 'L23'],
-		isFilled: false,
-		currentlyClashesWith: [],
-	},
-	TF1: {
-		clashesWith: ['TF1', 'L28', 'L29'],
-		isFilled: false,
-		currentlyClashesWith: [],
-	},
-	TG1: {
-		clashesWith: ['TG1', 'L5', 'L6'],
-		isFilled: false,
-		currentlyClashesWith: [],
-	},
-	TAA1: {
-		clashesWith: ['TAA1', 'L11', 'L12'],
-		isFilled: false,
-		currentlyClashesWith: [],
-	},
-	TCC1: {
-		clashesWith: ['TCC1', 'L23', 'L24'],
+		clashesWith: ['TC1', 'L25'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	TD1: {
-		clashesWith: ['TD1', 'L29', 'L30'],
+		clashesWith: ['TD1', 'L26'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TE1: {
+		clashesWith: ['TE1', 'L27'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TAA1: {
+		clashesWith: ['TAA1', 'L31'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TBB1: {
+		clashesWith: ['TBB1', 'L32'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TCC1: {
+		clashesWith: ['TCC1', 'L33'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 
 
 	A2: {
-		clashesWith: ['A2', 'L31', 'L32', 'L43', 'L44'],
+		clashesWith: ['A2', 'L4', 'L12'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	B2: {
-		clashesWith: ['B2', 'L37', 'L38', 'L49', 'L50'],
+		clashesWith: ['B2', 'L5', 'L16'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	C2: {
-		clashesWith: ['C2', 'L43', 'L44', 'L55', 'L56'],
+		clashesWith: ['C2', 'L6', 'L17'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	D2: {
-		clashesWith: ['D2', 'L49', 'L50', 'L33', 'L34'],
+		clashesWith: ['D2', 'L10', 'L18'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	E2: {
-		clashesWith: ['E2', 'L55', 'L56', 'L39', 'L40'],
-		isFilled: false,
-		currentlyClashesWith: [],
-	},
-	F2: {
-		clashesWith: ['F2', 'L31', 'L32', 'L45', 'L46'],
-		isFilled: false,
-		currentlyClashesWith: [],
-	},
-	G2: {
-		clashesWith: ['G2', 'L37', 'L38', 'L51', 'L52'],
+		clashesWith: ['E2', 'L11', 'L22'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	TA2: {
-		clashesWith: ['TA2', 'L57', 'L58'],
+		clashesWith: ['TA2', 'L23'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	TB2: {
-		clashesWith: ['TB2', 'L34', 'L35'],
+		clashesWith: ['TB2', 'L24'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	TC2: {
-		clashesWith: ['TC2', 'L40', 'L41'],
+		clashesWith: ['TC2', 'L28'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	TD2: {
-		clashesWith: ['TD2', 'L46', 'L47'],
+		clashesWith: ['TD2', 'L29'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	TE2: {
-		clashesWith: ['TE2', 'L52', 'L53'],
-		isFilled: false,
-		currentlyClashesWith: [],
-	},
-	TF2: {
-		clashesWith: ['TF2', 'L58', 'L59'],
-		isFilled: false,
-		currentlyClashesWith: [],
-	},
-	TG2: {
-		clashesWith: ['TG2', 'L35', 'L36'],
+		clashesWith: ['TE2', 'L30'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	TAA2: {
-		clashesWith: ['TAA2', 'L41', 'L42'],
+		clashesWith: ['TAA2', 'L34'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	TBB2: {
-		clashesWith: ['TBB2', 'L47', 'L48'],
+		clashesWith: ['TBB2', 'L35'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	TCC2: {
-		clashesWith: ['TCC2', 'L53', 'L54'],
-		isFilled: false,
-		currentlyClashesWith: [],
-	},
-	TDD2: {
-		clashesWith: ['TD2', 'L59', 'L60'],
+		clashesWith: ['TCC2', 'L36'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 
+	A3: {
+		clashesWith: ['A3', 'L37', 'L45'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	B3: {
+		clashesWith: ['B3', 'L38', 'L49'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	C3: {
+		clashesWith: ['C3', 'L39', 'L50'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	D3: {
+		clashesWith: ['D3', 'L43', 'L51'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	E3: {
+		clashesWith: ['E3', 'L44', 'L55'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TA3: {
+		clashesWith: ['TA3', 'L56'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TB3: {
+		clashesWith: ['TB3', 'L57'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TC3: {
+		clashesWith: ['TC3', 'L61'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TD3: {
+		clashesWith: ['TD3', 'L62'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TE3: {
+		clashesWith: ['TE3', 'L63'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TAA3: {
+		clashesWith: ['TAA3', 'L67'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TBB3: {
+		clashesWith: ['TBB3', 'L68'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TCC3: {
+		clashesWith: ['TCC3', 'L69'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
 
+	A4: {
+		clashesWith: ['A4', 'L40', 'L45'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	B4: {
+		clashesWith: ['B4', 'L41', 'L52'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	C4: {
+		clashesWith: ['C4', 'L42', 'L53'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	D4: {
+		clashesWith: ['D4', 'L46', 'L54'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	E4: {
+		clashesWith: ['E4', 'L47', 'L58'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TA4: {
+		clashesWith: ['TA4', 'L59'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TB4: {
+		clashesWith: ['TB4', 'L60'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TC4: {
+		clashesWith: ['TC4', 'L64'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TD4: {
+		clashesWith: ['TD4', 'L65'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TE4: {
+		clashesWith: ['TE4', 'L66'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TAA4: {
+		clashesWith: ['TAA4', 'L70'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TBB4: {
+		clashesWith: ['TBB4', 'L71'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	TCC4: {
+		clashesWith: ['TCC4', 'L72'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+
+	// LABS
+	// Monday Morning Labs
 	L1: {
 		clashesWith: ['L1', 'A1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L2: {
-		clashesWith: ['L2', 'F1'],
+		clashesWith: ['L2', 'B1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L3: {
-		clashesWith: ['L3', 'D1'],
+		clashesWith: ['L3', 'C1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L4: {
-		clashesWith: ['L4', 'TB1'],
+		clashesWith: ['L4', 'A2'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L5: {
-		clashesWith: ['L5', 'TG1', 'TB1'],
+		clashesWith: ['L5', 'B2'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L6: {
-		clashesWith: ['L6'],
+		clashesWith: ['L6', 'C2'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
+
+	// Tuesday Morning Labs
 	L7: {
-		clashesWith: ['L7', 'B1'],
+		clashesWith: ['L7', 'D1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L8: {
-		clashesWith: ['L8', 'G1'],
+		clashesWith: ['L8', 'E1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L9: {
-		clashesWith: ['L9', 'E1'],
+		clashesWith: ['L9', 'A1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L10: {
-		clashesWith: ['L10', 'TC1'],
+		clashesWith: ['L10', 'D2'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
-
 	L11: {
-		clashesWith: ['L11', 'TAA1', 'TC1'],
+		clashesWith: ['L11', 'E2'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L12: {
-		clashesWith: ['L12'],
+		clashesWith: ['L12', 'A2'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
+
+	// Wednesday Morning Labs
 	L13: {
-		clashesWith: ['L13', 'C1'],
+		clashesWith: ['L13', 'B1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L14: {
-		clashesWith: ['L14', 'A1'],
+		clashesWith: ['L14', 'C1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L15: {
-		clashesWith: ['L15', 'F1'],
+		clashesWith: ['L15', 'D1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L16: {
-		clashesWith: ['L16', 'V1'],
+		clashesWith: ['L16', 'B2'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
+	L17: {
+		clashesWith: ['L17', 'C2'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	L18: {
+		clashesWith: ['L18', 'D2'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+
+	// Thursday Morning Labs
 	L19: {
-		clashesWith: ['L19', 'D1'],
+		clashesWith: ['L19', 'E1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L20: {
-		clashesWith: ['L20', 'B1'],
+		clashesWith: ['L20', 'TA1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
-
 	L21: {
-		clashesWith: ['L21', 'G1'],
+		clashesWith: ['L21', 'TB1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L22: {
-		clashesWith: ['L22', 'TE1'],
+		clashesWith: ['L22', 'E2'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L23: {
-		clashesWith: ['L23', 'TCC1', 'TE1'],
+		clashesWith: ['L23', 'TA2'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L24: {
-		clashesWith: ['L24'],
+		clashesWith: ['L24', 'TB2'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
+
+	// Friday Morning Labs
 	L25: {
-		clashesWith: ['L25', 'E1'],
+		clashesWith: ['L25', 'TC1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L26: {
-		clashesWith: ['L26', 'C1'],
+		clashesWith: ['L26', 'TD1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L27: {
-		clashesWith: ['L27', 'TA1'],
+		clashesWith: ['L27', 'TE1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L28: {
-		clashesWith: ['L28', 'TF1'],
+		clashesWith: ['L28', 'TC2'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L29: {
-		clashesWith: ['L29', 'TD1', 'TF1'],
+		clashesWith: ['L29', 'TD2'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L30: {
-		clashesWith: ['L30'],
+		clashesWith: ['L30', 'TE2'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 
-
+	// Saturday Morning Labs
 	L31: {
-		clashesWith: ['L31', 'A2'],
+		clashesWith: ['L31', 'TAA1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L32: {
-		clashesWith: ['L32', 'F2'],
+		clashesWith: ['L32', 'TBB1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L33: {
-		clashesWith: ['L33', 'D2'],
+		clashesWith: ['L33', 'TCC1'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L34: {
-		clashesWith: ['L34', 'TB2'],
+		clashesWith: ['L34', 'TAA2'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L35: {
-		clashesWith: ['L35', 'TG2', 'TB2'],
+		clashesWith: ['L35', 'TBB2'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L36: {
-		clashesWith: ['L36', 'TG2'],
+		clashesWith: ['L36', 'TCC2'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
+
+	// Monday Afternoon Labs
 	L37: {
-		clashesWith: ['L37', 'B2'],
+		clashesWith: ['L37', 'A3'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L38: {
-		clashesWith: ['L38', 'G2'],
+		clashesWith: ['L38', 'B3'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L39: {
-		clashesWith: ['L39', 'E2'],
+		clashesWith: ['L39', 'C3'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L40: {
-		clashesWith: ['L40', 'TC2'],
+		clashesWith: ['L40', 'A4'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
-
 	L41: {
-		clashesWith: ['L41', 'TAA2', 'TC2'],
+		clashesWith: ['L41', 'B4'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L42: {
-		clashesWith: ['L42', 'TAA2'],
+		clashesWith: ['L42', 'C4'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
+
+	// Tuesday Afternoon Labs
 	L43: {
-		clashesWith: ['L43', 'C2'],
+		clashesWith: ['L43', 'D3'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L44: {
-		clashesWith: ['L44', 'A2'],
+		clashesWith: ['L44', 'E3'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L45: {
-		clashesWith: ['L45', 'F2'],
+		clashesWith: ['L45', 'A3'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L46: {
-		clashesWith: ['L46', 'TD2'],
+		clashesWith: ['L46', 'D4'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L47: {
-		clashesWith: ['L47', 'TD2', 'TBB2'],
+		clashesWith: ['L47', 'E4'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L48: {
-		clashesWith: ['L48', 'TBB2'],
+		clashesWith: ['L48', 'A4'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
+
+	// Wednesday Afternoon Labs
 	L49: {
-		clashesWith: ['L49', 'D2'],
+		clashesWith: ['L49', 'B3'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L50: {
-		clashesWith: ['L50', 'B2'],
+		clashesWith: ['L50', 'C3'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
-
 	L51: {
-		clashesWith: ['L51', 'G2'],
+		clashesWith: ['L51', 'D3'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L52: {
-		clashesWith: ['L52', 'TE2'],
+		clashesWith: ['L52', 'B4'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L53: {
-		clashesWith: ['L53', 'TCC2', 'TE2'],
+		clashesWith: ['L53', 'C4'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L54: {
-		clashesWith: ['L54', 'TCC2'],
+		clashesWith: ['L54', 'D4'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
+
+	// Thursday Afternoon Labs
 	L55: {
-		clashesWith: ['L55', 'E2'],
+		clashesWith: ['L55', 'E3'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L56: {
-		clashesWith: ['L56', 'C2'],
+		clashesWith: ['L56', 'TA3'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L57: {
-		clashesWith: ['L57', 'TA2'],
+		clashesWith: ['L57', 'TB3'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L58: {
-		clashesWith: ['L58', 'TF2'],
+		clashesWith: ['L58', 'E4'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L59: {
-		clashesWith: ['L59', 'TDD2', 'TF2'],
+		clashesWith: ['L59', 'TA4'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 	L60: {
-		clashesWith: ['L60', 'TDD2'],
+		clashesWith: ['L60', 'TB4'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
 
-
-	V1: {
-		clashesWith: ['V1', 'L16'],
+	// Friday Afternoon Labs
+	L61: {
+		clashesWith: ['L61', 'TC3'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
-	V2: {
-		clashesWith: ['V2'],
+	L62: {
+		clashesWith: ['L62', 'TD3'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
-	V3: {
-		clashesWith: ['V3'],
+	L63: {
+		clashesWith: ['L63', 'TE3'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
-	V4: {
-		clashesWith: ['V4'],
+	L64: {
+		clashesWith: ['L64', 'TC4'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
-	V5: {
-		clashesWith: ['V5'],
+	L65: {
+		clashesWith: ['L65', 'TD4'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
-	V6: {
-		clashesWith: ['V6'],
+	L66: {
+		clashesWith: ['L66', 'TE4'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},
-	V7: {
-		clashesWith: ['V7'],
+	
+	// Saturday Afternoon Labs
+	L67: {
+		clashesWith: ['L67', 'TAA3'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	L68: {
+		clashesWith: ['L68', 'TBB3'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	L69: {
+		clashesWith: ['L69', 'TCC3'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	L70: {
+		clashesWith: ['L70', 'TAA4'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	L71: {
+		clashesWith: ['L71', 'TBB4'],
+		isFilled: false,
+		currentlyClashesWith: [],
+	},
+	L72: {
+		clashesWith: ['L72', 'TCC4'],
 		isFilled: false,
 		currentlyClashesWith: [],
 	},

c1b1439c03a80f4678bcba07f7898af0ac25eddb
Namit Nathwani <namsnath@gmail.com>
2020-08-21 16:15:01 +0530

    feat: adds Provider and store to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import {
 	BrowserRouter as Router, Switch,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 
 import ReactGA from 'react-ga';
 
@@ -11,10 +13,16 @@ import PrivateRoute from './components/PrivateRoute';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 
+import rootReducer from './reducers/rootReducer';
+
 ReactGA.initialize('UA-156974674-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = () => {
+	const store = configureStore({
+		reducer: rootReducer,
+	});
+
 	const [authenticated, setAuthenticated] = useState(false);
 
 	const isAuthenticated = () => API.get('/account')
@@ -32,28 +40,30 @@ const App = () => {
 	}, []);
 
 	return (
-		<Router>
-			<Switch>
-				{/* <Route exact path="/login" component={Login} /> */}
-				<PrivateRoute
-					path="/login"
-					redirect="/"
-					component={Login}
-					isAuthenticated={!authenticated}
-				/>
+		<Provider store={store}>
+			<Router>
+				<Switch>
+					{/* <Route exact path="/login" component={Login} /> */}
+					<PrivateRoute
+						path="/login"
+						redirect="/"
+						component={Login}
+						isAuthenticated={!authenticated}
+					/>
 
-				<PrivateRoute
-					path="/"
-					redirect="/login"
-					component={Dashboard}
-					isAuthenticated={authenticated}
-					handleUnauth={() => {
-						API.get('/logout');
-						setAuthenticated(false);
-					}}
-				/>
-			</Switch>
-		</Router>
+					<PrivateRoute
+						path="/"
+						redirect="/login"
+						component={Dashboard}
+						isAuthenticated={authenticated}
+						handleUnauth={() => {
+							API.get('/logout');
+							setAuthenticated(false);
+						}}
+					/>
+				</Switch>
+			</Router>
+		</Provider>
 	);
 };
 

8d1e1d322bbd0eefae8fd78e7ecf87d5e325f45c
Namit Nathwani <namsnath@gmail.com>
2020-10-02 00:00:03 +0530

    feat: adds tests for changeTimetable

--- client/src/reducers/timetable.spec.ts
+++ client/src/reducers/timetable.spec.ts
@@ -1,7 +1,10 @@
 import timetable, {
 	initialState,
 	addTimetable,
+	changeTimetable,
 } from './timetable';
+
+import clashmap from '../constants/ClashMap';
 
 describe('timetable reducer', () => {
 	it('should handle initial state', () => {
@@ -46,6 +49,149 @@ describe('timetable reducer', () => {
 				// To check that active timetable does not change
 				// and that a duplicate isn't added
 				expect(timetable(initState, addAction)).toStrictEqual(finalState);
+			});
+		});
+
+		describe(`${changeTimetable.type}`, () => {
+			const course1 = {
+				_id: '5f59ffe5cee64f31983a29fe',
+				code: 'ARB1001',
+				course_type: 'TH',
+				credits: 3,
+				faculty: 'MOHD SAQIB',
+				slot: 'A1+TA1',
+				venue: 'SJT823',
+				simpleCourseType: 'Theory',
+				title: 'Arabic for Beginners',
+				timetableName: 'Default',
+			};
+
+			const course2 = {
+				_id: '5f59ffe5cee64f31983a29ff',
+				code: 'ARB1001',
+				course_type: 'TH',
+				credits: 3,
+				faculty: 'MOHD SAQIB',
+				slot: 'B1+TB1',
+				venue: 'SJT403',
+				simpleCourseType: 'Theory',
+				title: 'Arabic for Beginners',
+				timetableName: 'Sample',
+			};
+
+			const timestamp = new Date(Date.now()).toISOString();
+
+			const initState = {
+				active: 'Default',
+				names: ['Default', 'Sample'],
+				filledSlots: ['A1', 'TA1'],
+				data: [
+					{ ...course1 },
+					{ ...course2 },
+				],
+				timestamp,
+				creditCount: 3,
+				syncing: false,
+				clashmap: {
+					...clashmap,
+					A1: {
+						...clashmap.A1,
+						isFilled: true,
+						currentlyClashesWith: ['A1'],
+					},
+					TA1: {
+						...clashmap.TA1,
+						isFilled: true,
+						currentlyClashesWith: ['TA1'],
+					},
+					L1: {
+						...clashmap.L1,
+						currentlyClashesWith: ['A1'],
+					},
+					L14: {
+						...clashmap.L14,
+						currentlyClashesWith: ['A1'],
+					},
+					L27: {
+						...clashmap.L27,
+						currentlyClashesWith: ['TA1'],
+					},
+				},
+			};
+
+			const finalState = {
+				active: 'Sample',
+				names: ['Default', 'Sample'],
+				filledSlots: ['B1', 'TB1'],
+				data: [
+					{ ...course1 },
+					{ ...course2 },
+				],
+				timestamp,
+				creditCount: 3,
+				syncing: false,
+				clashmap: {
+					...clashmap,
+					B1: {
+						...clashmap.B1,
+						isFilled: true,
+						currentlyClashesWith: ['B1'],
+					},
+					TB1: {
+						...clashmap.TB1,
+						isFilled: true,
+						currentlyClashesWith: ['TB1'],
+					},
+					L4: {
+						...clashmap.L4,
+						currentlyClashesWith: ['TB1'],
+					},
+					L5: {
+						...clashmap.L5,
+						currentlyClashesWith: ['TB1'],
+					},
+					L7: {
+						...clashmap.L7,
+						currentlyClashesWith: ['B1'],
+					},
+					L20: {
+						...clashmap.L20,
+						currentlyClashesWith: ['B1'],
+					},
+				},
+			};
+
+			it('should change to an existing timetable', () => {
+				expect.hasAssertions();
+
+				const action = {
+					type: changeTimetable.type,
+					payload: 'Sample',
+				};
+
+				expect(timetable(initState, action)).toStrictEqual(finalState);
+			});
+
+			it('should not change to a non-existent timetable', () => {
+				expect.hasAssertions();
+
+				const action = {
+					type: changeTimetable.type,
+					payload: 'NonExistent',
+				};
+
+				expect(timetable(initState, action)).toStrictEqual(initState);
+			});
+
+			it('should do nothing on changing to active timetable', () => {
+				expect.hasAssertions();
+
+				const action = {
+					type: changeTimetable.type,
+					payload: 'Default',
+				};
+
+				expect(timetable(initState, action)).toStrictEqual(initState);
 			});
 		});
 	});

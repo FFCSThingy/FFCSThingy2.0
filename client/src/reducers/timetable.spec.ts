02f0d34f00335f493cf45fe435eb9d7713f92bfd
Namit Nathwani <namsnath@gmail.com>
2020-10-02 00:13:46 +0530

    update: moves const definitions to top-level describe

--- client/src/reducers/timetable.spec.ts
+++ client/src/reducers/timetable.spec.ts
@@ -2,18 +2,100 @@ import timetable, {
 	initialState,
 	addTimetable,
 	changeTimetable,
+	removeTimetable,
+	renameTimetable,
 } from './timetable';
 
 import clashmap from '../constants/ClashMap';
 
 describe('timetable reducer', () => {
+	const timestamp = new Date(Date.now()).toISOString();
+			const course1 = {
+				_id: '5f59ffe5cee64f31983a29fe',
+				code: 'ARB1001',
+				course_type: 'TH',
+				credits: 3,
+				faculty: 'MOHD SAQIB',
+				slot: 'A1+TA1',
+				venue: 'SJT823',
+				simpleCourseType: 'Theory',
+				title: 'Arabic for Beginners',
+				timetableName: 'Default',
+			};
+			const course2 = {
+				_id: '5f59ffe5cee64f31983a29ff',
+				code: 'ARB1001',
+				course_type: 'TH',
+				credits: 3,
+				faculty: 'MOHD SAQIB',
+				slot: 'B1+TB1',
+				venue: 'SJT403',
+				simpleCourseType: 'Theory',
+				title: 'Arabic for Beginners',
+				timetableName: 'Sample',
+			};
+	const course1Clashmap = {
+					...clashmap,
+					A1: {
+						...clashmap.A1,
+						isFilled: true,
+						currentlyClashesWith: ['A1'],
+					},
+					TA1: {
+						...clashmap.TA1,
+						isFilled: true,
+						currentlyClashesWith: ['TA1'],
+					},
+					L1: {
+						...clashmap.L1,
+						currentlyClashesWith: ['A1'],
+					},
+					L14: {
+						...clashmap.L14,
+						currentlyClashesWith: ['A1'],
+					},
+					L27: {
+						...clashmap.L27,
+						currentlyClashesWith: ['TA1'],
+					},
+			};
+	const course2Clashmap = {
+					...clashmap,
+					B1: {
+						...clashmap.B1,
+						isFilled: true,
+						currentlyClashesWith: ['B1'],
+					},
+					TB1: {
+						...clashmap.TB1,
+						isFilled: true,
+						currentlyClashesWith: ['TB1'],
+					},
+					L4: {
+						...clashmap.L4,
+						currentlyClashesWith: ['TB1'],
+					},
+					L5: {
+						...clashmap.L5,
+						currentlyClashesWith: ['TB1'],
+					},
+					L7: {
+						...clashmap.L7,
+						currentlyClashesWith: ['B1'],
+					},
+					L20: {
+						...clashmap.L20,
+						currentlyClashesWith: ['B1'],
+					},
+	};
+
 	it('should handle initial state', () => {
 		expect.hasAssertions();
 		expect(timetable(undefined, { type: '' })).toStrictEqual(initialState);
 	});
 
 	describe('timetable actions', () => {
-		describe(`${addTimetable.type}`, () => {
+		describe(`${addTimetable}`, () => {
 			const addAction = {
 				type: addTimetable.type,
 				payload: 'Sample',
@@ -52,35 +134,7 @@ describe('timetable reducer', () => {
 			});
 		});
 
-		describe(`${changeTimetable.type}`, () => {
-			const course1 = {
-				_id: '5f59ffe5cee64f31983a29fe',
-				code: 'ARB1001',
-				course_type: 'TH',
-				credits: 3,
-				faculty: 'MOHD SAQIB',
-				slot: 'A1+TA1',
-				venue: 'SJT823',
-				simpleCourseType: 'Theory',
-				title: 'Arabic for Beginners',
-				timetableName: 'Default',
-			};
-
-			const course2 = {
-				_id: '5f59ffe5cee64f31983a29ff',
-				code: 'ARB1001',
-				course_type: 'TH',
-				credits: 3,
-				faculty: 'MOHD SAQIB',
-				slot: 'B1+TB1',
-				venue: 'SJT403',
-				simpleCourseType: 'Theory',
-				title: 'Arabic for Beginners',
-				timetableName: 'Sample',
-			};
-
-			const timestamp = new Date(Date.now()).toISOString();
-
+		describe(`${changeTimetable}`, () => {
 			const initState = {
 				active: 'Default',
 				names: ['Default', 'Sample'],
@@ -92,31 +146,7 @@ describe('timetable reducer', () => {
 				timestamp,
 				creditCount: 3,
 				syncing: false,
-				clashmap: {
-					...clashmap,
-					A1: {
-						...clashmap.A1,
-						isFilled: true,
-						currentlyClashesWith: ['A1'],
-					},
-					TA1: {
-						...clashmap.TA1,
-						isFilled: true,
-						currentlyClashesWith: ['TA1'],
-					},
-					L1: {
-						...clashmap.L1,
-						currentlyClashesWith: ['A1'],
-					},
-					L14: {
-						...clashmap.L14,
-						currentlyClashesWith: ['A1'],
-					},
-					L27: {
-						...clashmap.L27,
-						currentlyClashesWith: ['TA1'],
-					},
-				},
+				clashmap: course1Clashmap,
 			};
 
 			const finalState = {
@@ -130,35 +160,7 @@ describe('timetable reducer', () => {
 				timestamp,
 				creditCount: 3,
 				syncing: false,
-				clashmap: {
-					...clashmap,
-					B1: {
-						...clashmap.B1,
-						isFilled: true,
-						currentlyClashesWith: ['B1'],
-					},
-					TB1: {
-						...clashmap.TB1,
-						isFilled: true,
-						currentlyClashesWith: ['TB1'],
-					},
-					L4: {
-						...clashmap.L4,
-						currentlyClashesWith: ['TB1'],
-					},
-					L5: {
-						...clashmap.L5,
-						currentlyClashesWith: ['TB1'],
-					},
-					L7: {
-						...clashmap.L7,
-						currentlyClashesWith: ['B1'],
-					},
-					L20: {
-						...clashmap.L20,
-						currentlyClashesWith: ['B1'],
-					},
-				},
+				clashmap: course2Clashmap,
 			};
 
 			it('should change to an existing timetable', () => {

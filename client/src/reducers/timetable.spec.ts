f240d79fb0c5ee8252c1e1011067dc4ab79d5490
Namit Nathwani <namsnath@gmail.com>
2020-10-01 21:37:17 +0530

    update: cleans up addTimetable tests

--- client/src/reducers/timetable.spec.ts
+++ client/src/reducers/timetable.spec.ts
@@ -11,63 +11,41 @@ describe('timetable reducer', () => {
 
 	describe('timetable actions', () => {
 		describe(`${addTimetable.type}`, () => {
+			const addAction = {
+				type: addTimetable.type,
+				payload: 'Sample',
+			};
+
 			it('should handle new timetable (no duplicates)', () => {
 				expect.hasAssertions();
 
-				expect(
-					timetable(
-						undefined,
-						{
-							type: addTimetable.type,
-							payload: 'Sample',
-						},
-					),
-				).toStrictEqual({
+				const finalState = {
 					...initialState,
 					active: 'Sample',
 					names: ['Default', 'Sample'],
-				});
+				};
+
+				expect(timetable(undefined, addAction)).toStrictEqual(finalState);
 			});
 
 			it('should handle new timetable (new name already present)', () => {
 				expect.hasAssertions();
 
-				// To check that active timetable does not change
-				expect(
-					timetable(
-						{
-							...initialState,
-							active: 'Default',
-							names: ['Default', 'Sample'],
-						},
-						{
-							type: addTimetable.type,
-							payload: 'Sample',
-						},
-					),
-				).toStrictEqual({
+				const initState = {
 					...initialState,
 					active: 'Default',
 					names: ['Default', 'Sample'],
-				});
+				};
 
-				expect(
-					timetable(
-						{
-							...initialState,
-							active: 'Sample',
-							names: ['Default', 'Sample'],
-						},
-						{
-							type: addTimetable.type,
-							payload: 'Sample',
-						},
-					),
-				).toStrictEqual({
+				const finalState = {
 					...initialState,
-					active: 'Sample',
-					names: ['Default', 'Sample'],
-				});
+					active: 'Default', // Active timetable should remain same
+					names: ['Default', 'Sample'], // Sample shouldn't be added to the list again
+				};
+
+				// To check that active timetable does not change
+				// and that a duplicate isn't added
+				expect(timetable(initState, addAction)).toStrictEqual(finalState);
 			});
 		});
 	});

f1abcae7ab1237d64b26f4d6453f25ad89b95924
Namit Nathwani <namsnath@gmail.com>
2020-10-23 17:01:13 +0530

    update: timetable selector exports
    - Removes default export

--- client/src/selectors/timetable.ts
+++ client/src/selectors/timetable.ts
@@ -2,12 +2,10 @@ import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from '../app/rootReducer';
 
 export const selectTimetable = (state: RootState) => state.timetable.data;
-const selectActiveTimetableName = (state: RootState) => state.timetable.active;
+export const selectActiveTimetableName = (state: RootState) => state.timetable.active;
 export const selectTimetableTimestamp = (state: RootState) => state.timetable.timestamp;
 
-const selectFilteredTimetable = createSelector(
+export const selectFilteredTimetable = createSelector(
 	[selectTimetable, selectActiveTimetableName],
 	(timetable, active) => timetable.filter((v) => v.timetableName === active),
 );
-
-export default selectFilteredTimetable;
